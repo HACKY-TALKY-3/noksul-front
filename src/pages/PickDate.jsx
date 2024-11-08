44503881c2ea8bdd0536fc104ad8c549fe7dea14
Jaewoo Kim <jaeooo.kim@gmail.com>
2024-11-09 01:41:23 +0900

    bezier-react 설정 및 라우트 추가

---
+++ src/pages/PickDate.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PickDate() {
+    return (<h1>Pick Date</h1>);
+}
+
+export default PickDate;
---
+++ src/pages/SetTime.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SetTime() {
+    return (< h1>Set Time</h1>);
+}
+
+export default SetTime;
